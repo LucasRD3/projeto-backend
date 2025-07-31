bb6223ed086ba9aaccfeecd15e44e19f6a115ef4
LucasRD3 <95994537+LucasRD3@users.noreply.github.com>
2025-07-31 01:38:32 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'https://gleaming-meerkat-0fdcfc.netlify.app/',
+    origin: process.env.FRONTEND_URL || 'https://gleaming-meerkat-0fdcfc.netlify.app,
     credentials: true
 }));
 app.use(express.json());
